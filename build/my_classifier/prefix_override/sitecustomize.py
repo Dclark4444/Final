22e31f94f9b89ebf1490ef9bef9d20a1596b16b3
hanimn12 <hanimn@uchicago.edu>
2025-12-04 22:00:04 -0600

    adding

--- build/my_classifier/prefix_override/sitecustomize.py
+++ build/my_classifier/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/clarkd26/intro_robo_ws/src/Final/install/my_classifier'
+    sys.prefix = sys.exec_prefix = '/home/hanimn/intro_robo_ws/src/Final/install/my_classifier'
--- install/setup.zsh
+++ install/setup.zsh
@@ -22,10 +22,10 @@ _colcon_prefix_chain_zsh_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/clarkd26/intro_robo_ws/src/install"
+COLCON_CURRENT_PREFIX="/home/hanimn/intro_robo_ws/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/clarkd26/intro_robo_ws/install"
+COLCON_CURRENT_PREFIX="/home/hanimn/intro_robo_ws/src/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
