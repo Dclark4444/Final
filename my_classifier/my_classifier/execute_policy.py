09c9548e684b6a553c3877c4e6b1192e30fc6e4f
David (Trey) Clark <deltadown123@gmail.com>
2025-12-10 20:33:18 -0600

    Update execute_policy.py

--- my_classifier/my_classifier/execute_policy.py
+++ my_classifier/my_classifier/execute_policy.py
@@ -64,6 +64,8 @@ class ExecutePolicy(Node):
         try:
             self.get_logger().info(f"0")
             frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+            if frame is None:
+                self.get_logger().info(f"frame failed")
             self.get_logger().info(f"1")
             img = cv2.resize(frame, IMG_SIZE)
             self.get_logger().info(f"2")
